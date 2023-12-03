78106ffd01fd229ccb799d6e9c9104f4b482e415
shan <shanlakmal223@gmail.com>
2023-12-03 12:13:12 +0530

    changed code in appjs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 app.listen(3000, () => {
-      console.log("Server is running at http://localhost:3000");
+      console.log("Server is running at shan http://localhost:3000");
     });
